c2fcde4ee63b262c59dc5b62db2789899becb885
TurboJax07 <turbojax07@gmail.com>
2024-08-02 16:28:42 -0400

    Build constants go brrr

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Forte-2-5";
+  public static final String MAVEN_NAME = "ForteOffseason2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "74f1f3554a5fa3cff31fd1498132c95c385e6d00";
-  public static final String GIT_DATE = "2024-08-02 11:30:38 EDT";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "a61bfc361ac30bc1925ec462a566f65dc2d677ea";
+  public static final String GIT_DATE = "2024-08-02 14:11:08 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-08-02 14:02:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1722621779087L;
+  public static final String BUILD_DATE = "2024-08-02 15:53:08 EDT";
+  public static final long BUILD_UNIX_TIME = 1722628388797L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
