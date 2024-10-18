d3a4a8707328591d90a430f102dc2b82e8cbbb9b
apoorva <97716935+carokhan@users.noreply.github.com>
2024-10-18 06:22:47 -0400

    stupid

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Forte-2-5";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 83;
-  public static final String GIT_SHA = "cd5533efceedea228db5f5c1abfb4d489944d593";
-  public static final String GIT_DATE = "2024-10-16 05:10:03 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-10-17 07:03:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1729163021410L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 85;
+  public static final String GIT_SHA = "332353998183b9d2f9fc779572058bac7a2cf12e";
+  public static final String GIT_DATE = "2024-10-17 08:15:01 EDT";
+  public static final String GIT_BRANCH = "vision";
+  public static final String BUILD_DATE = "2024-10-17 22:40:28 EDT";
+  public static final long BUILD_UNIX_TIME = 1729219228598L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
