26872d882927fd9cf21b7d43dcf1a4b295ff444b
apoorva <97716935+carokhan@users.noreply.github.com>
2024-10-26 16:57:25 -0400

    Update at 'Sat Oct 26 16:57:25 EDT 2024'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Forte-2-5";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 99;
-  public static final String GIT_SHA = "bfd3cd138ac06363b2db8f40560ca5cbd45e1d25";
-  public static final String GIT_DATE = "2024-10-26 16:50:50 EDT";
+  public static final int GIT_REVISION = 100;
+  public static final String GIT_SHA = "c7326251b7888dc93b4b2e6b52fdd8e561f25633";
+  public static final String GIT_DATE = "2024-10-26 16:53:36 EDT";
   public static final String GIT_BRANCH = "event_2024vagi2";
-  public static final String BUILD_DATE = "2024-10-26 16:50:51 EDT";
-  public static final long BUILD_UNIX_TIME = 1729975851169L;
+  public static final String BUILD_DATE = "2024-10-26 16:53:36 EDT";
+  public static final long BUILD_UNIX_TIME = 1729976016344L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -276,9 +276,9 @@ public class RobotContainer {
         .whileTrue(
             Commands.parallel(
                     m_pivot.setPivotTarget(() -> Units.degreesToRadians(40)),
-                    m_shooter.setRPM(() -> -1200, 1.0),
-                    m_feeder.setRPM(() -> -1200),
-                    m_intake.setRollerRPM(() -> -1200))
+                    m_shooter.setRPM(() -> -1300, 1.0),
+                    m_feeder.setRPM(() -> -1300),
+                    m_intake.setRollerRPM(() -> -1300))
                 .until(
                     () ->
                         (m_feeder.feederBeambreakObstructed()
