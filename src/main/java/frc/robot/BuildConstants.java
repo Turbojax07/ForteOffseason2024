86c6b5629bc9b1aa6110c2daccbe04a2a06a4d49
apoorva <97716935+carokhan@users.noreply.github.com>
2024-10-26 13:42:25 -0400

    Update at 'Sat Oct 26 13:42:25 EDT 2024'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Forte-2-5";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 94;
-  public static final String GIT_SHA = "3cda90de91fb1b0090279df599becdebdcdab3f2";
-  public static final String GIT_DATE = "2024-10-26 13:41:37 EDT";
+  public static final int GIT_REVISION = 95;
+  public static final String GIT_SHA = "54b05dd83bb044e04f1f06dd475c9df594b7d24d";
+  public static final String GIT_DATE = "2024-10-26 13:42:02 EDT";
   public static final String GIT_BRANCH = "event_2024vagi2";
-  public static final String BUILD_DATE = "2024-10-26 13:41:37 EDT";
-  public static final long BUILD_UNIX_TIME = 1729964497810L;
+  public static final String BUILD_DATE = "2024-10-26 13:42:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1729964523070L;
   public static final int DIRTY = 0;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
@@ -14,6 +14,7 @@ import com.revrobotics.SparkPIDController.ArbFFUnits;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import frc.robot.Constants.RobotMap;
+import frc.robot.Constants.ShooterConstants;
 
 /** Add your docs here. */
 public class ShooterIOSparkMax implements ShooterIO {
